92d4b4bbd81baeec383bb5d6fb7eb38748571966
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-05-28 22:02:25 +0800

    new

--- passenger counter/index.js
+++ passenger counter/index.js
@@ -1,6 +1,13 @@
 //document.getElementById("counter-el").innerText = 5;
 
-let count = 0;
-let myAge = 28;
+//let count = 0;
+//let myAge = 28;
 
-console.log(myAge);
+//console.log(myAge);
+
+let firstBatch = 5;
+let secondBatch = 8;
+
+let count = firstBatch + secondBatch;
+
+console.log(count);
