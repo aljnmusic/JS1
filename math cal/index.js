aa3539c59908f829ab0157d7083098e387fc934e
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-02 06:12:05 -0700

    new

---
+++ math cal/index.js
@@ -0,0 +1,21 @@
+let n1 = document.getElementById("num1")
+let n2 = document.getElementById("num2")
+let total =  document.getElementById("total-el")
+
+
+function add(){
+    let sum = Number(n1.innerText) + Number(n2.innerText)
+    total.innerText = "SUM: " + sum
+}
+
+function subtract(){
+    let sum = Number(n1.innerText) - Number(n2.innerText)
+    total.innerText = "SUM: " + sum
+}
+
+function multiply(){
+    let sum = Number(n1.innerText) * Number(n2.innerText)
+    total.innerText = "SUM: " + sum
+}
+
+function
