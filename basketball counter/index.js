552b18fd7c5a0f9987f99ec4573ca8b17d97df1f
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-02 06:51:43 -0700

    new

--- basketball counter/index.js
+++ basketball counter/index.js
@@ -15,3 +15,21 @@ function add3(){
     score += 3;
     point.innerText = score;
 }
+
+let guestpoint = document.getElementById("point1")
+let guestscore = 0
+
+function gadd1(){
+    guestscore += 1;
+    guestpoint.innerText = guestscore;
+}
+
+function gadd2(){
+    guestscore += 2;
+    guestpoint.innerText = guestscore;
+}
+
+function gadd3(){
+    guestscore += 3;
+    guestpoint.innerText = guestscore;
+}
