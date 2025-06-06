170e1db95f83fedf18214f0c50f7bfce957d640f
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-06 08:26:39 -0700

    new

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,8 +1,23 @@
 let firstCard = 10
 let secondCard = 12
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 
 function startGame(){
+    if(sum <= 20){
+        message = "You're Not a Black Jack!"
+    }
+    else if(sum === 20){
+        message = "You are black jack!"
+        hasBlackJack = true
+    }
+    else{
+        message = "You are out of the game!"
+        isAlive = false
+    }
 
+    console.log(message)
 }
