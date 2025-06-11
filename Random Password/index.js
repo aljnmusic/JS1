cb9366fd75004361cf1734f1be59dc3e008aa749
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-11 07:06:40 -0700

    61125950

--- Random Password/index.js
+++ Random Password/index.js
@@ -1,1 +1,13 @@
 const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}<>?"
+let newEl = document.getElementById("new-el")
+let passLength = 15
+function randomPassword(){
+    let password = ""
+    if (passLength){
+        for(let i = 0; i < char.length; i++){
+            let charindex = Math.floor(Math.random() * char.length)
+            password += char[charindex]
+        }
+    }
+    newEl.textContent = password.substring(0, 15)
+}
