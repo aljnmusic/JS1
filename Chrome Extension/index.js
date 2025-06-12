7808dfabcc40b3eb2b5219a086ae0cc3a9229cc3
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-12 06:16:42 -0700

    61225916

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function () {
-    let elementEl = inputEl.valueOf
+    let elementEl = inputEl.value
     myLeads.push(elementEl)
+    console.log(myLeads)
 })
