e3169d82ee43bd58605b218e876c0167eda109e8
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-20 07:04:28 -0700

    620251004

--- Unit Converter/index.js
+++ Unit Converter/index.js
@@ -2,16 +2,25 @@ let inputEl = document.getElementById("input-el")
 let convertBtn = document.getElementById("convert-btn")
 let lengthEl  = document.getElementById("length-el")
 let weightEl = document.getElementById("weight-el")
+let curEl = document.getElementById("cur-el")
 
 const meterToFeet =  3.281
 const feetToMeter = 0.3047
 const literToGallon = 0.264172
 const gallonToLitter = 3.78541
+const phpToUsdRate = 0.0179
+const usdToPhpRate = 55.84
 
 convertBtn.addEventListener("click", function(){
-    let baseValue = inputEl.value
+    let baseValue = parseFloat(inputEl.value)
 
-    lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet | ${baseValue} feet = ${baseValue * feetToMeter} meter`
+    if (isNaN(baseValue)) {
+        alert("Please enter a valid base value!")
+    }
 
-    weightEl.textContent = `${baseValue} Liters = ${baseValue * literToGallon} Gallons | ${baseValue} Gallons = ${baseValue * gallonToLitter} Litter`
+    lengthEl.textContent = `${baseValue} meter = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue * feetToMeter).toFixed(3)} meter`
+
+    weightEl.textContent = `${baseValue} Liters = ${(baseValue * literToGallon).toFixed(3)} Gallons | ${baseValue} Gallons = ${(baseValue * gallonToLitter).toFixed(3)} Litter`
+
+    curEl.textContent = `${baseValue} Pesos = ${(baseValue * phpToUsdRate).toFixed(3)} USD | ${baseValue} USD = ${(baseValue * usdToPhpRate).toFixed(3)} Pesos`
 })
