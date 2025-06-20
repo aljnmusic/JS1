1b5b701fa689fe7284abac8e3467db66d0d65a17
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-20 06:45:44 -0700

    62025945

--- Unit Converter/index.js
+++ Unit Converter/index.js
@@ -1,2 +1,14 @@
 let inputEl = document.getElementById("input-el")
 let convertBtn = document.getElementById("convert-btn")
+let lengthEl  = document.getElementById("length-el")
+
+const meterToFeet =  3.281
+const feetToMeter = 0.3047
+const literToGallon = 0.264172
+const gallonToLitter = 3.78541
+
+convertBtn.addEventListener("click", function(){
+    let baseValue = inputEl.value
+
+    lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet | ${baseValue} feet = ${baseValue * feetToMeter} meter`
+})
