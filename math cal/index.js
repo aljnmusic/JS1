11e91dccf6d8f4fc2e45eeb2bce9efc892b9e872
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-24 06:52:08 -0700

    62325859

--- math cal/index.js
+++ math cal/index.js
@@ -4,21 +4,21 @@ let total =  document.getElementById("total-el")
 
 
 function add(){
-    let sum = Number(n1.innerText) + Number(n2.innerText)
+    let sum = Number(n1.value) + Number(n2.value)
     total.innerText = "SUM: " + sum
 }
 
 function subtract(){
-    let sum = Number(n1.innerText) - Number(n2.innerText)
-    total.innerText = "SUM: " + sum
+    let sum = Number(n1.value) - Number(n2.value)
+    total.innerText = "Difference: " + sum
 }
 
 function multiply(){
-    let sum = Number(n1.innerText) * Number(n2.innerText)
-    total.innerText = "SUM: " + sum
+    let sum = Number(n1.value) * Number(n2.value)
+    total.innerText = "Product: " + sum
 }
 
 function divide(){
-    let sum = Number(n1.innerText) / Number(n2.innerText)
-    total.innerText = "SUM: " + sum
+    let sum = Number(n1.value) / Number(n2.value)
+    total.innerText = "Quotient: " + sum.toFixed(2)
 }
