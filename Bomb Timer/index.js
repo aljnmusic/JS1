ce484ba3c1c2b4730280c5dd5e71e57377453c46
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-26 08:50:28 -0700

    626251145

--- Bomb Timer/index.js
+++ Bomb Timer/index.js
@@ -1,1 +1,13 @@
 let timeEl = document.getElementsByClassName("time");
+let countBtn = document.getElementsByClassName("counter-el");
+let tickingEl = document.getElementsByClassName("ticking")
+
+function startTimer() {
+    let currentTime = timeEl.value
+
+    if (currentTime < 0) {
+        setInterval(function () {
+
+        })
+    }
+}
