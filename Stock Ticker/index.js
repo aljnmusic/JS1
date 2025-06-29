64a6e8a59e2fb7f506ad6a965babdeb86964dd6c
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-06-29 07:47:21 -0700

    629251017

--- Stock Ticker/index.js
+++ Stock Ticker/index.js
@@ -5,6 +5,8 @@ setInterval(function (){
     renderStockTicker(stockData)
 }, 1500)
 
+let prevPrice = null
+
 function renderStockTicker(stockData) {
     const stockDisplayName = document.getElementById('name')
     const stockDisplaySymbol = document.getElementById('symbol')
@@ -14,11 +16,22 @@ function renderStockTicker(stockData) {
 
     const {name, symbol, price, time} = stockData
 
+    const priorPrice = price > prevPrice ? 'green.svg' : price <  prevPrice ? 'red.svg' : 'grey.svg'
+
+    const priceIconElement = document.createElement('img')
+    priceIconElement.src = `svg/${priorPrice}`
+    priceIconElement.alt = 'Price Direction Icon'
+    priceIconElement.style.width = '15px'
+    priceIconElement.style.height = '15px'
+    stockDisplayPriceIcon.innerHTML = ''
+    stockDisplayPriceIcon.appendChild(priceIconElement)
+
     stockDisplayName.innerText = name
     stockDisplaySymbol.innerText = symbol
     stockDisplayPrice.innerText = price
     stockDisplayTime.innerText = time
 
+    prevPrice = price
 }
 
 /*
