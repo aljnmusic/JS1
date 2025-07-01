4e3760d41cf47cbc46ef30332610edd0a141b32f
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-07-01 22:59:37 +0800

    71251017

--- Shopping List/index.js
+++ Shopping List/index.js
@@ -1,1 +1,21 @@
-let
+let listEl = document.getElementById('list');
+let itemInput = document.getElementById('item-input');
+let addItemBtn = document.getElementById('add-item-btn');
+
+const shoppingList = []
+
+addItemBtn.addEventListener('click', function(){
+    shoppingList.push(itemInput.value)
+    render()
+    itemInput.value = ''
+})
+
+function render(){
+    let html = ''
+    for (let item of shoppingList) {
+        html += `<li class="list-item">${item}</li>`
+    }
+    list.innerHTML = html
+}
+
+render()
