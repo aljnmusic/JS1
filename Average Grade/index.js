9fe9fb0c00a8d3ff595ae52a09c738608881013e
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-07-03 23:17:51 +0800

    7325

---
+++ Average Grade/index.js
@@ -0,0 +1,8 @@
+let gradeInput = document.getElementById('input-grade')
+let submitBtn =  document.getElementById('submit')
+let averageResult = document.getElementById('result')
+
+const studentGrade = []
+submitBtn.addEventListener('click', function(){
+
+})
