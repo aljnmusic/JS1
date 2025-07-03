ad9c9b8612467964a7c6e5c5cceffc2fc4f2e058
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-07-04 00:10:41 +0800

    7325

--- Average Grade/index.js
+++ Average Grade/index.js
@@ -1,34 +1,68 @@
 let gradeInput = document.getElementById('input-grade')
+let units = document.getElementById('input-units')
 let submitBtn =  document.getElementById('submit')
 let averageResult = document.getElementById('result')
 
 const studentGrade = []
+const studentUnits = []
 submitBtn.addEventListener('click', function(){
     let student = gradeInput.value
+    let studentUni = units.value
+
     if (student){
         studentGrade.push(Number(student))
+        studentUnits.push(Number(studentUni))
+
         gradeInput.value = ''
+        units.value = ''
         renderGrade()
     }
 })
 
+// function convertToGWA(percentage) {
+//     if (percentage >= 90) return 1.0
+//     if (percentage >= 85) return 1.5
+//     if (percentage >= 80) return 2.0
+//     if (percentage >= 75) return 2.5
+//     if (percentage >= 70) return 3.0
+//     if (percentage >= 65) return 3.5
+//     if (percentage >= 60) return 4.0
+//     if (percentage >= 50) return 4.5
+//     return 5.0  // Below 50% is 5.0 (Failing)
+// }
+
 function renderGrade(){
     let html = ''
 
-    studentGrade.forEach(function(grade){
+    studentGrade.forEach(function(grade, index){
         html += `
-                    <li>${grade}</li>
+                    <li>${grade}, ${studentUnits[index]} units</li>
                  `
     })
 
-    let total = studentGrade.reduce(function(prev, curr){
-        return prev + curr
-    }, 0)
+    let totalWeightScore = 0
+    let totalUnits = 0
+    let averageGrade = 0
 
-    let average = total / studentGrade.length
+    studentGrade.forEach(function(grade, index){
+        averageGrade += grade
+        totalWeightScore += grade * studentUnits[index]
+        totalUnits += studentUnits[index]
+    })
 
-    html += `Total Grade: ${total} <br>
-             Average Grade: ${average.toFixed(2)}%`
+    let gwa = totalWeightScore / totalUnits
+
+    switch(gwa){
+        case 'gwa > 90':
+            return 1.0
+    }
+
+    html += `
+        <br>Total Weighted Score: ${totalWeightScore} <br>
+        Average Grade: ${(averageGrade / studentGrade.length).toFixed(2)}% <br>
+        GWA: ${gwa.toFixed(2)}%
+    `
+
 
     averageResult.innerHTML = html
 }
