9d1ae6b10f5398daf4292186121b418d3ee2a752
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-07-03 23:38:52 +0800

    7325

--- Average Grade/index.js
+++ Average Grade/index.js
@@ -4,5 +4,31 @@ let averageResult = document.getElementById('result')
 
 const studentGrade = []
 submitBtn.addEventListener('click', function(){
-
+    let student = gradeInput.value
+    if (student){
+        studentGrade.push(Number(student))
+        gradeInput.value = ''
+        renderGrade()
+    }
 })
+
+function renderGrade(){
+    let html = ''
+
+    studentGrade.forEach(function(grade){
+        html += `
+                    <li>${grade}</li>
+                 `
+    })
+
+    let total = studentGrade.reduce(function(prev, curr){
+        return prev + curr
+    }, 0)
+
+    let average = total / studentGrade.length
+
+    html += `Total Grade: ${total} <br>
+             Average Grade: ${average.toFixed(2)}%`
+
+    averageResult.innerHTML = html
+}
