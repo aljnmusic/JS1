c5676ef364ee1695d697c352bd6091ac403e7b9d
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-07-04 22:45:34 +0800

    7425

--- ContactSearch/index.js
+++ ContactSearch/index.js
@@ -1,4 +1,4 @@
-import {contactsArr} from "./contactsData";
+import {contactsArr} from './contactsData'
 
 /*
 Challenge:
@@ -9,9 +9,24 @@ Challenge:
 
 const patternSearchInput = document.getElementById('pattern-search-input')
 const patternSearchSubmit = document.getElementById('pattern-search-submit')
-const contactDisplay = document.getElementById('contact-display')
+
+patternSearchSubmit.addEventListener('click', function(){
+    findMatchingContacts(contactsArr, patternSearchInput.value)
+})
+
+function findMatchingContacts(contactsArr, pattern) {
+    const regex = new RegExp(pattern, 'i')
+    contactsArr.filter(function(contact){
+        return regex.test(contact.name)
+    })
+        .forEach(function(contact){
+            renderContact(contact)
+        })
+}
 
 function renderContact(contactObj) {
+    const {name, email, phone} =  contactObj
+    const contactDisplay = document.getElementById('contact-display')
     const contactCard = document.createElement('aside')
     contactCard.classList.add('contact-card')
     /*
@@ -19,4 +34,16 @@ function renderContact(contactObj) {
         The name, email and phone details can be placed in
         'p' elements and placed inside contact-card.
     */
+    const nameEl = document.createElement('p')
+    const emailEl = document.createElement('p')
+    const phoneEl = document.createElement('p')
+    nameEl.innerText = name
+    emailEl.innerText = email
+    phoneEl.innerText = phone
+
+    contactCard.appendChild(nameEl)
+    contactCard.appendChild(emailEl)
+    contactCard.appendChild(phoneEl)
+
+    contactDisplay.appendChild(contactCard)
 }
