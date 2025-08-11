b31fc8c85bfc71fd5f39ac07d758f54419e96dfc
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-11 22:53:05 +0800

    Created a login form

--- imageAsync/index.js
+++ imageAsync/index.js
@@ -1,51 +1,51 @@
-function getImagePromise(url) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const img = new Image()
-            img.src = url
-            img.alt = 'scenic image'
-            img.addEventListener('load', () => resolve(img))
-            img.addEventListener('error', () => reject(new Error(`Failed to load image: ${url}`)))
-        }, 500)
-    })
-}
-
-const images = [
-    'https://scrimba.com/links/advancedjs-resources-images-scenic1',
-    'https://scrimba.com/links/advancedjs-resources-images-scenic2',
-    'https://scrimba.com/links/advancedjs-resources-images-scenic3'
-]
-
-async function preloadImages(imageUrlsArr) {
-    const imgContainer = document.getElementById('img-container')
-    const uploadContainer = document.getElementById('upload-container')
-
-    try{
-        const image1 = getImagePromise(imageUrlsArr[0])
-        const image2 = getImagePromise(imageUrlsArr[1])
-        const image3 = getImagePromise(imageUrlsArr[2])
-        const results = await Promise.all([image1, image2, image3])
-        console.log('ALl images loaded successfully!')
-        uploadContainer.style.display = 'none'
-
-        for (let i = 0; i < results.length; i++) {
-            imgContainer.appendChild(results[i])
-        }
-    } catch (error) {
-        console.error(error)
+    function getImagePromise(url) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const img = new Image()
+                img.src = url
+                img.alt = 'scenic image'
+                img.addEventListener('load', () => resolve(img))
+                img.addEventListener('error', () => reject(new Error(`Failed to load image: ${url}`)))
+            }, 500)
+        })
     }
-    /*
-    Challenge:
-      1. Create an array of promises using getImagePromise.
-      2. Save the results of calling all of those promises
-         in one go to a const 'results'.
-      3. If the promises resolve:
-         - log "All images loaded successfully!".
-         - hide 'uploadContainer'
-         - iterate over the results and render them to imgContainer.
-      4. If the promises reject:
-         - catch and log the error.
-    */
-}
 
-document.getElementById('submit-imgs').addEventListener('click', () => preloadImages(images))
+    const images = [
+        'https://scrimba.com/links/advancedjs-resources-images-scenic1',
+        'https://scrimba.com/links/advancedjs-resources-images-scenic2',
+        'https://scrimba.com/links/advancedjs-resources-images-scenic3'
+    ]
+
+    async function preloadImages(imageUrlsArr) {
+        const imgContainer = document.getElementById('img-container')
+        const uploadContainer = document.getElementById('upload-container')
+
+        try{
+            const image1 = getImagePromise(imageUrlsArr[0])
+            const image2 = getImagePromise(imageUrlsArr[1])
+            const image3 = getImagePromise(imageUrlsArr[2])
+            const results = await Promise.all([image1, image2, image3])
+            console.log('ALl images loaded successfully!')
+            uploadContainer.style.display = 'none'
+
+            for (let i = 0; i < results.length; i++) {
+                imgContainer.appendChild(results[i])
+            }
+        } catch (error) {
+            console.error(error)
+        }
+        /*
+        Challenge:
+          1. Create an array of promises using getImagePromise.
+          2. Save the results of calling all of those promises
+             in one go to a const 'results'.
+          3. If the promises resolve:
+             - log "All images loaded successfully!".
+             - hide 'uploadContainer'
+             - iterate over the results and render them to imgContainer.
+          4. If the promises reject:
+             - catch and log the error.
+        */
+    }
+
+    document.getElementById('submit-imgs').addEventListener('click', () => preloadImages(images))
