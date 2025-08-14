ce71d053dc6b8d92b540de8e0e68631dc83c31aa
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-14 20:26:42 +0800

    added an upload input field

--- NotesCloud/upload.js
+++ NotesCloud/upload.js
@@ -1,3 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
+
+const firebaseConfig = {
+    databaseUrl: "https://notescloud-d8d47-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database =  getDatabase(app)
+
+console.log(app)
+
 let formEl = document.getElementById('form')
 let courseCodeInput =  document.getElementById('course-code-input')
 let topicTitleInput = document.getElementById('topic-title-input')
