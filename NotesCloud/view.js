5c0a25f3576c51b5000c839a0408db05e0204361
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-18 14:22:53 +0800

    view js to render data

---
+++ NotesCloud/view.js
@@ -0,0 +1,75 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore, getDocs, collection } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-WrZ6hS35vXo4OwgmgzxmpJQuf1QKJdo",
+    authDomain: "notescloud-d8d47.firebaseapp.com",
+    projectId: "notescloud-d8d47",
+    storageBucket: "notescloud-d8d47.appspot.com",
+    messagingSenderId: "852197125275",
+    appId: "1:852197125275:web:8b32da22fe46b2406d2a3e"
+};
+
+
+// Initialize Firebase app and Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Cloudinary info
+const cloudName = "drrtevw5u";
+const uploadPreset = "notescloud_unsigned";
+
+let formEl = document.getElementById('form')
+let searchInputEl = document.getElementById('search-input')
+let submitBtn = document.getElementById('submit-button')
+let resultContainerEl = document.getElementById('result-container')
+
+document.addEventListener("DOMContentLoaded", async() => {
+    await loadInitialData()
+})
+
+async function loadInitialData() {
+    const collectionRef = collection(db, "notes")
+
+    try{
+        const snapshot = await getDocs(collectionRef)
+        const result = []
+
+        snapshot.forEach(doc => {
+            result.push(doc.data())
+        })
+
+        renderResults(result)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function renderResults(notes) {
+    let html = ''
+
+    notes.forEach(data => {
+        const fileURL = data.fileURL;
+        const isImage = /\.(jpg|jpeg|png|gif)$/i.test(fileURL);
+        const title = data.topicTitle || "Untitled";
+        const course = data.courseCode || "N/A";
+        const createdAt = data.createdAt?.toDate?.().toLocaleString() || "";
+
+
+        html += `
+            <div class="p-4 border rounded mb-4 shadow-sm bg-white">
+                <h3 class="text-lg font-semibold mb-1">${title} (${course})</h3>
+                        <p class="text-sm text-gray-500 mb-2">${createdAt}</p>
+                        ${isImage
+                    ? `<img src="${fileURL}" alt="${title}" class="max-w-xs rounded" />`
+                    : `<a href="${fileURL}" target="_blank" class="text-blue-600 underline">📄 View File</a>`
+                }
+            </div>
+        `
+    })
+
+    resultContainerEl.innerHTML = html;
+}
+
