a1e4f3ee4e46b116832354c790d4025e22b53b2b
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-29 22:47:20 +0800

    feat: js script

---
+++ todo list/todo.js
@@ -0,0 +1,8 @@
+function newList(){
+    let newList = document.createElement("li");
+    let myInput = document.getElementById("myInput");
+    let t = document.createTextNode(myInput.value);
+
+
+
+}
