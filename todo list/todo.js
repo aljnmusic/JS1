d48e8068ddc1b1038c2eeab88af7d5f20a5c504b
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-29 23:50:51 +0800

    feat: js script

--- todo list/todo.js
+++ todo list/todo.js
@@ -1,8 +1,19 @@
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", newList);
+// window.addEventListener("DOMContentLoaded", loadListFromLocalStorage)
+
 function newList(){
-    let newList = document.createElement("li");
+    let li = document.createElement("li");
     let myInput = document.getElementById("myInput");
     let t = document.createTextNode(myInput.value);
+    li.appendChild(t);
 
+    if(myInput.value.length == 0){
+        alert("Input empty");
+    } else {
+        document.getElementById("myUl").appendChild(li);
+    }
 
-
+    document.getElementById("myInput").value = "";
 }
