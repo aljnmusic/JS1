4ffe84cfc650c182ff72d77ff241e50db15937ca
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-30 21:16:48 +0800

    feat: js script

--- todo list/todo.js
+++ todo list/todo.js
@@ -9,11 +9,32 @@ function newList(){
     let t = document.createTextNode(myInput.value);
     li.appendChild(t);
 
-    if(myInput.value.length == 0){
+    if(myInput.value.length === 0){
         alert("Input empty");
     } else {
         document.getElementById("myUl").appendChild(li);
     }
 
     document.getElementById("myInput").value = "";
+
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7")
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for(let i = 0; i < close.length; i++){
+        close[i].onclick = function(){
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
+
+
+let list = document.getElementById("li");
+list.addEventListener("click", function(ev){
+    if(ev.target.tagName === "LI"){
+        ev.target.classList.toggle("checked")
+    }
+});
