c96bc8caecd2e3d92159ccdeaf426a00c2691411
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-05 00:05:53 +0800

    feat: calculator app, added js script

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,7 +1,33 @@
-let input = document.getElementById("display")
 let current = ''
 let previous = ''
 let currentOperation = ''
 
+function appendNumber(number){
+    current += number
+    document.getElementById("display").value = `${previous} ${currentOperation} ${current}`
+}
+
+function appendOperation(operation){
+    if(operation === ''){
+        return
+    }
 
 
+    previous = current
+    currentOperation = operation
+    current = ''
+    document.getElementById('display').value = `${previous} (${operation})`
+
+}
+
+function calculateTotal(){
+
+}
+
+
+function clearInput(){
+    current = ''
+    previous = ''
+    currentOperation = ''
+    document.getElementById("display").value = ''
+}
