35aaa9088ba047364fd75593f3f8d02036489544
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-07 23:02:15 +0800

    feat: created a js for random number and output them

--- GuessRandomNumber/guess.js
+++ GuessRandomNumber/guess.js
@@ -2,43 +2,15 @@ let inputEl = document.getElementById('inputNum')
 let startBtn = document.getElementById('startbtn')
 let message = document.getElementById('message')
 
+let guessLeft;
+let random
 
-function validateInput(){
-    let userInput = Number(inputEl.value)
-
-    if(!Number.isInteger(userInput)){
-        message.textContent = "Please enter a valid number!"
-        return false
-    }
-
-    return true
+function startGame(){
+    random = Math.floor(Math.random() * 100)
+    guessLeft = 3
+    inputEl.textContent = ''
+    inputEl.disable = false;
+    startBtn.textContent = 'Start Game'
 }
-startBtn.addEventListener('click', () => {
-    let random = Math.floor(Math.random() * 100)
-    let guess = 3;
 
-    if(!validateInput()){
-        return
-    }
-
-    for(let i = 1; i <= guess; i++) {
-        if (Number(inputEl.value) === random) {
-            message.textContent = "You're " + inputEl.value + "is correct"
-            break
-        }
-        else if(Number(inputEl.value) < random) {
-            message.textContent = "Too low! Try again!"
-        }
-        else if(Number(inputEl.value) > random) {
-            message.textContent = "Too high! Try again!"
-        }
-
-
-        if(i === guess){
-            message.textContent = `Game over! The correct number is ${random}`
-        }
-
-    }
-
-
-})
+startGame()
