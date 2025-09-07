113c83eda8f0735178b1ecd0ed820ac7e3990e49
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-07 23:28:41 +0800

    feat: final js

--- GuessRandomNumber/guess.js
+++ GuessRandomNumber/guess.js
@@ -6,11 +6,58 @@ let guessLeft;
 let random
 
 function startGame(){
-    random = Math.floor(Math.random() * 100)
+    random = Math.floor(Math.random() * 10)
     guessLeft = 3
-    inputEl.textContent = ''
+    inputEl.value = ''
     inputEl.disable = false;
-    startBtn.textContent = 'Start Game'
+    startBtn.textContent = 'Guess'
+    message.textContent = ''
+
 }
+
+function checkGuess(){
+    let input = Number(inputEl.value)
+
+    if(!Number.isInteger(input) || input < 1 || input > 100) {
+        message.textContent = "Enter a valid number from 1-100"
+        return
+    }
+
+    guessLeft--
+
+    if(input === random){
+        message.textContent = "You guessed the correct number!"
+        endGame()
+    }
+    else if(guessLeft === 0){
+        message.textContent = `Game over! Correct number is ${random}`
+        endGame()
+    }
+    else if(input < random){
+        message.textContent = "Too low! Try again"
+    }
+    else
+    {
+        message.textContent = "Too high! try again!"
+    }
+
+    inputEl.value = ''
+
+}
+
+function endGame(){
+    inputEl.disable = true;
+    startBtn.textContent = 'Play again?'
+
+}
+
+startBtn.addEventListener('click', () => {
+    if (startBtn.textContent === 'Play again?') {
+        startGame()
+    }
+    else{
+        checkGuess()
+    }
+})
 
 startGame()
