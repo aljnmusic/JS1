99b4af4026463044b4a11c03e380d71aa9fb1869
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-07 22:20:26 +0800

    feat: created a js for random number and output them

--- GuessRandomNumber/guess.js
+++ GuessRandomNumber/guess.js
@@ -2,13 +2,34 @@ let inputEl = document.getElementById('inputNum')
 let startBtn = document.getElementById('startbtn')
 let message = document.getElementById('message')
 
-startBtn.addEventListener('click', () => {
-    let random = Math.floor(Math.random() * 10)
 
-    if (Number(inputEl.value) === random) {
-        message.textContent = "You're " + inputEl.value + "is correct"
+function validateInput(){
+    let userInput = Number(inputEl.value)
+
+    if(!Number.isInteger(userInput)){
+        message.textContent = "Please enter a valid number!"
+        return false
     }
-    else if (Number(inputEl.value) !== random) {
-        message.textContent = "You're number incorrect"
+
+    return true
+}
+startBtn.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * 5)
+    let guess = 3;
+
+    validateInput()
+
+    for(let i = 0; i <= guess; i++) {
+        if (Number(inputEl.value) === random) {
+            message.textContent = "You're " + inputEl.value + "is correct"
+            return
+        }
+        else if (Number(inputEl.value) !== random) {
+            message.textContent = "You're number is incorrect"
+        }
+
+        guess++;
     }
+
+
 })
