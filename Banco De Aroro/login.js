8b065707805a16c4a283dc785923d920bf6de120
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-08 21:49:05 +0800

    feat: created a login js

---
+++ Banco De Aroro/login.js
@@ -0,0 +1,4 @@
+let nameInput = document.getElementById('name');
+let passwordInput = document.getElementById('password');
+let signInBtn =  document.getElementById('signIn');
+let form = document.getElementById('loginForm');
