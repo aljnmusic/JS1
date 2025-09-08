40cc6f489984e776a193418390d4ae2a9f73aa06
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-08 22:06:07 +0800

    feat: created a login js

--- Banco De Aroro/login.js
+++ Banco De Aroro/login.js
@@ -1,4 +1,14 @@
+import { credentials, createName } from "./credentials.js";
+
 let nameInput = document.getElementById('name');
 let passwordInput = document.getElementById('password');
 let signInBtn =  document.getElementById('signIn');
-let form = document.getElementById('loginForm');
+let formEl = document.getElementById('loginForm');
+
+formEl.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    console.log("Form submitted!");
+
+    createName(nameInput.value);
+
+})
