a17a6b1222234c1382e2d82a296881973f5812e0
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-12 19:12:33 +0800

    feat: changed the font style of the saved localStorage name

--- Banco De Aroro/home.js
+++ Banco De Aroro/home.js
@@ -3,4 +3,4 @@ import { credentials, createName } from "./credentials.js";
 let userEl = document.getElementById('username')
 let storedName = localStorage.getItem('username');
 
-userEl.textContent = storedName;
+userEl.textContent = `Welcome ${storedName}`;
