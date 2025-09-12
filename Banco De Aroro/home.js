c0121f708b8f360c8e64dbd09b795a1865af197a
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-12 21:27:28 +0800

    feat: logic for displaying appropriate div

--- Banco De Aroro/home.js
+++ Banco De Aroro/home.js
@@ -3,4 +3,13 @@ import { credentials, createName } from "./credentials.js";
 let userEl = document.getElementById('username')
 let storedName = localStorage.getItem('username');
 
+let viewImg = document.getElementById('viewImg');
+let saveImg = document.getElementById('saveImg');
+let withdrawImg = document.getElementById('withdrawImg');
+let logoutImg = document.getElementById('logoutImg');
+
+let viewDiv = document.getElementById('viewDiv');
+let depositDiv = document.getElementById('depositDiv');
+let withdrawDiv = document.getElementById('withdrawDiv');
+
 userEl.textContent = `Welcome ${storedName} !`;
